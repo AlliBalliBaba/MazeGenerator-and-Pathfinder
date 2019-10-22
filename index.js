a4b7b3fc5750587791d24948d04824e6335fbf2d
Alliballibaba <alexander.stecher@rolmail.net>
2019-10-22 12:22:35 +0200

    changed layout

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 var currentMaze;
-var simplePathfinder;
-var aStarPathfinder;
-var mazePathfinder;
+var pathfinder;
+const pathfinders= [SimpleSolver,AStarSolver,MazeRouter];
 
 let canvas;
-var canvasX = 400;
-var canvasY = 400;
+const canvasX = 500, canvasY = 500;
 
-var mazeX = 50;
-var mazeY = 50;
-
-var mazeStart = 3;
-var mazeComplexity = 300;
+var mazeX = 50, mazeY = 50;
+var mazeStart = 3, mazeComplexity = 300;
 var gridSizeX, gridSizeY = 1;
-var solveCounter = 0;
-var solveSpeed = 5;
-var active = false;
+var solveCounter = 0, solveSpeed = 5, active = false;
 
-//set up canvas and constants
+
+
+//set up canvas
 function setup() {
     ReadHtmlValues();
-    canvas = createCanvas(canvasX * 3 + 30, canvasY);
+    canvas = createCanvas(canvasX, canvasY);
     canvas.parent('canvas1');
     background(245);
     gridSizeX = canvasX / mazeX;
@@ -31,16 +26,14 @@ function setup() {
     mazePathfinder = null;
 }
 
-//p5 function (is called every frame)
+// function called every frame
 function draw() {
     if (active) {
         solveSpeed = document.getElementById("speedSlider").value * 0.1;
         solveCounter += solveSpeed;
         if (solveCounter > 1) {
             for (var i = 1; i < solveCounter; i++) {
-                simplePathfinder.solve();
-                aStarPathfinder.solve();
-                mazePathfinder.solve();
+                pathfinder.solve();
             }
             solveCounter = 0;
         }
@@ -57,10 +50,11 @@ function createNewMaze() {
 }
 
 function startSolver() {
+    let id=document.getElementById("finderselect").value;
+    console.log(id)
+    if (id<0){ return; }
     document.getElementById("solveButton").disabled = true;
-    simplePathfinder = new SimpleSolver(mazeStart, currentMaze.endPos);
-    aStarPathfinder = new AStarSolver(mazeStart, currentMaze.endPos);
-    mazePathfinder = new MazeRouter(mazeStart, currentMaze.endPos);
+    pathfinder = new pathfinders[id](mazeStart, currentMaze.endPos);
     active = true;
 }
 
@@ -69,7 +63,5 @@ function ReadHtmlValues() {
     mazeY = Math.floor(document.getElementById("sizeSlider").value);
     mazeX > 75 ? shift = 1 : shift = 2;
     mazeComplexity = Math.floor(100 - document.getElementById("complexitySlider").value) * mazeX * mazeX / 200;
-    document.getElementById("length1").innerHTML = "--";
-    document.getElementById("length2").innerHTML = "--";
-    document.getElementById("length3").innerHTML = "--";
+    document.getElementById("length").innerHTML = "-- <br> --";
 }
