0fa4dd3796d3ebd4385948e5bbb7841f6a0cc367
Alliballibaba <alexander.stecher@rolmail.net>
2019-11-06 03:09:53 +0100

    UI changes

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ let canvas;
 var canvasX = 500,
     canvasY = 500;
 
-var mazeX = 50,
-    mazeY = 50;
-var mazeStart = 3,
-    mazeComplexity = 300;
+var mazeX = 25,
+    mazeY = 25;
+var startX = 0.5,
+    startY = 0,
+    endX = 0.5,
+    endY = mazeY - 1;
+mazeComplexity = 300;
 var gridSizeX, gridSizeY = 1;
 var solveCounter = 0,
     solveSpeed = 5,
@@ -19,7 +22,6 @@ var solveCounter = 0,
 function setup() {
     readHtmlValues();
     if (screen.width - 20 < canvasX) {
-        console.log(screen.width)
         canvasX = screen.width - 20;
         canvasY = screen.width - 20;
     }
@@ -36,35 +38,54 @@ function setup() {
 
 // function called every frame
 function draw() {
-    if (active) {
-        solveSpeed = document.getElementById("speedSlider").value * 0.1;
-        solveCounter += solveSpeed;
-        if (solveCounter > 1) {
-            for (var i = 1; i < solveCounter; i++) {
-                pathfinder.solve();
-            }
-            solveCounter = 0;
+    if (!active) return;
+    solveSpeed = document.getElementById("speedSlider").value * 0.1;
+    solveCounter += solveSpeed;
+    if (solveCounter > 1) {
+        for (var i = 1; i < solveCounter; i++) {
+            pathfinder.solve();
         }
+        solveCounter = 0;
     }
 }
 
 function createNewMaze() {
     setup();
     document.getElementById("solveButton").disabled = false;
-    currentMaze = new Maze(mazeX, mazeY, mazeStart)
+    currentMaze = new Maze(mazeX, mazeY, Math.floor(startX * mazeX), startY, Math.floor(endX * mazeX), mazeY - 1);
     currentMaze.generate();
     drawMaze(currentMaze);
-
+    disableButton(false, "solveButton");
+    disableButton(true, "resetButton");
 }
 
 function startSolver() {
     let id = document.getElementById("finderselect").value;
-    if (id < 0) {
-        return;
-    }
-    document.getElementById("solveButton").disabled = true;
-    pathfinder = new pathfinders[id](mazeStart, currentMaze.endPos);
+    if (id < 0 || !currentMaze) return;
+    disableButton(true, "solveButton");
+    disableButton(false, "resetButton");
+    pathfinder = new pathfinders[id](currentMaze.start, currentMaze.end);
     active = true;
+}
+
+function resetSolver() {
+    setup();
+    drawMaze(currentMaze);
+    disableButton(false, "solveButton");
+    disableButton(true, "resetButton");
+}
+
+function mouseClicked() {
+    if (mouseX > canvasX / mazeX && mouseX < canvasX - canvasX / mazeX && mouseY > 0 && mouseY < canvasY) {
+        if (mouseY < canvasY / mazeY * 2) {
+            startX = mouseX / (canvasX + 0.5);
+            createNewMaze();
+        }
+        if (mouseY > canvasY - canvasY / mazeY * 2) {
+            endX = mouseX / (canvasX + 0.5);
+            createNewMaze();
+        }
+    }
 }
 
 function readHtmlValues() {
